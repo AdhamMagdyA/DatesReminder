c198b29e5d027d3f4c872def163afe4ac167ed33
AdhamMagdyA <adhammagdy.dev@gmail.com>
2023-05-05 08:15:46 +0200

    built the main UI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import { Button, Col, Container, Row } from "react-bootstrap";
+
 function App() {
   return (
-    <div className="App">
-      <h1> MyCode </h1>
+    <div className="App font">
+      <Container className="py-5 justify-content-center">
+        <Row className="justify-content-center">
+          <Col sm="8">لديك 6 مواعيد</Col>
+        </Row>
+        <Row className=" justify-content-center">
+          <Col sm="8">
+            <div className="window"></div>
+          </Col>
+        </Row>
+        <Row className=" justify-content-center">
+          <Col sm="8" className="d-flex justify-content-between m-2">
+            <Button variant="primary">مسح الكل</Button>
+            <Button variant="primary">عرض البيانات </Button>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
